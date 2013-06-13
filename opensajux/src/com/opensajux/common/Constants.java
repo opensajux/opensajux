c96cfee342c2ebd4073a3047ebc9dde1150f56cd
smsajid <smsajid786@gmail.com>
2013-06-13 10:34:08 +0530

    update version to 0.6.0

--- opensajux/src/com/opensajux/common/Constants.java
+++ opensajux/src/com/opensajux/common/Constants.java
@@ -1,6 +1,6 @@
 package com.opensajux.common;
 
 public interface Constants {
-	public String APP_NAME = "Open-Sajux/0.5.0";
+	public String APP_NAME = "Open-Sajux/0.6.0";
 	public String JDO_PERSISTENCE_MANAGER = "jdoPersistenceManager";
 }
